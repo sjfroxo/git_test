f82efd84817d1617ef8b4be001b0d523a7c80eea
sjfroxo <kholyavski@mail.ru>
2025-08-14 15:06:50 +0300

    new third commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function test()
+    {
+        $a = 5;
+        $b = 10;
+        $sum = $a + $b;
+
+        return $sum * 10;
+    }
 }
